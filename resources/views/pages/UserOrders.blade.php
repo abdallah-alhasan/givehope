740b81b8e4cb86a9c2c6b3d4e6700a01c220bc7f
ola-kanakri <ola.kanakri12@gmail.com>
2022-06-13 22:44:24 +0300

    UserOrders design

--- resources/views/pages/UserOrders.blade.php
+++ resources/views/pages/UserOrders.blade.php
@@ -5,11 +5,11 @@
 
 <div class="block-31" style="position: relative;">
     <div class="loop-block-31 ">
-       <div class="block-30 block-30-sm item" style="background-image: url({{url('images/donate.jpg')}});" data-stellar-background-ratio="0.5">
+       {{-- <div class="block-30 block-30-sm item" style="background-image: url({{url('images/donate.jpg')}});" data-stellar-background-ratio="0.5"> --}}
         <div class="container">
           <div class="row align-items-center justify-content-center text-center">
             <div class="col-md-7">
-              <h2 class="heading">Better To Give Than To Receive</h2>
+                <br><br><br><h2 class="heading">Better To Give Than To Receive</h2>
             </div>
           </div>
         </div>
@@ -92,7 +92,7 @@
 
 
     </div>
-  </div> <!-- .section -->
+  {{-- </div> <!-- .section --> --}}
 <!--.featured-donate -->
 
 @endsection
