c062c0a02856c3e508300be1310b2a725f30b186
yousefalhindawi <eng.yousefalhindawi@gmail.com>
2022-06-13 20:08:07 +0300

    main

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -118,7 +118,8 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link"
+                                        href="{{ '/signup'}}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
